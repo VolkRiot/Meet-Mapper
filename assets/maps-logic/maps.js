030ee111cc0d3ef641e19da18bd921ef2ced8b32
Mikhail Metrikin <metrikin@gmail.com>
2017-03-19 16:52:30 -0700

    Simple map constructor

--- assets/maps-logic/maps.js
+++ assets/maps-logic/maps.js
@@ -1,11 +1,49 @@
-function initMap() {
-  var uluru = {lat: -25.363, lng: 131.044};
-  var map = new google.maps.Map(document.getElementById('map-container'), {
-    zoom: 4,
-    center: uluru
-  });
-  var marker = new google.maps.Marker({
-    position: uluru,
-    map: map
-  });
+function GMapInterface(container) {
+  this.startLoc = {lat: 37.7919221, lng: -122.393739};
+  this.mapOptions = {
+    zoom: 16,
+    center: this.startLoc,
+    draggableCursor:'crosshair'
+  };
+  this.map;
+  this.newMarker;
+  this.initMap(container);
 }
+
+GMapInterface.prototype.initMap = function(contId) {
+
+  this.map = new google.maps.Map(document.getElementById(contId), this.mapOptions);
+
+};
+
+GMapInterface.prototype.createMarker = function(latLong) {
+
+  if(this.newMarker){
+    this.newMarker.setMap(null);
+  }
+
+  var marker = new google.maps.Marker({
+      position: latLong,
+      custom: "Something new can go here"
+    });
+
+  this.newMarker = marker;
+
+  marker.setMap(this.map);
+
+};
+
+
+// Start document here....
+
+$(document).ready(function () {
+
+  var Map = new GMapInterface('map-container');
+  Map.createMarker(Map.startLoc);
+
+  Map.map.addListener('click', function(event) {
+
+    Map.createMarker(event.latLng);
+  });
+
+});
