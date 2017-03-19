cbef4309a8ab5419d75bc8cd92a8b10a2dba33d5
Mikhail Metrikin <metrikin@gmail.com>
2017-03-18 20:28:01 -0700

    Map intergration and clean html start

--- assets/maps-logic/maps.js
+++ assets/maps-logic/maps.js
@@ -1,6 +1,6 @@
 function initMap() {
   var uluru = {lat: -25.363, lng: 131.044};
-  var map = new google.maps.Map(document.getElementById('map'), {
+  var map = new google.maps.Map(document.getElementById('map-container'), {
     zoom: 4,
     center: uluru
   });
