29a03b7f32f1ca67855d00ce2d167df8794e2e90
Mikhail Metrikin <metrikin@gmail.com>
2017-03-20 11:29:43 -0700

    Fixed a bug

--- assets/main.js
+++ assets/main.js
@@ -1,8 +1,17 @@
+//TODO: (DEVELOPER) Remove this debugging global
+var GlobalObj;
+////////////////////////////////////////////////////////////
 
 $(document).ready(function () {
 
   var Map = new GMapInterface('map-container');
   Map.createMarker(Map.startLoc);
   Map.queryUserLocation();
+
+  Map.map.addListener('click', function(event) {
+
+    Map.createMarker(event.latLng);
+
+  });
 
 });
