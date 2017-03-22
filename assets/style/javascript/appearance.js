0b7b98355e5694b5fc61a041fa881a3445a184d9
IamZombiez <dereksiu216@gmail.com>
2017-03-21 17:34:51 -0700

    Initial SIte Files

--- assets/style/javascript/appearance.js
+++ assets/style/javascript/appearance.js
@@ -1,14 +1,6 @@
-/* Set the width of the side navigation to 250px Opening Function */
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-}
-
-/* Set the width of the side navigation to 0. Close Function */
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
-
-/* Side Nav Open On Click */
-$("#slideoutNav").on("click", function () {
-	openNav()
+$(function() {
+  $('.datepicker').pickadate({
+    selectMonths: true,
+    selectYears: 15
+  });
 });
