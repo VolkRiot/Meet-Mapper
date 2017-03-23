a25b102d332044d8e5d5a38a47ebb5558d27a60d
IamZombiez <dereksiu216@gmail.com>
2017-03-22 21:35:31 -0700

    Added Buttons for event form and upcoming events table. JS for buttons

--- assets/style/javascript/appearance.js
+++ assets/style/javascript/appearance.js
@@ -6,11 +6,25 @@ $(function() {
     selectYears: 15
   });
 
+// timepicker function
  $('.timepicker').pickatime({
     autoclose: false,
     twelvehour: true,
     default: '14:20:00'
   });
+
+});
+
+
+$("#add-popout").on("click", function() {
+	$("#upcoming-events").hide()
+	$("#add-eventform").show()
+
+});
+
+$("#upcoming-popout").on("click", function() {
+	$("#add-eventform").hide()
+	$("#upcoming-events").show()
 
 });
 
