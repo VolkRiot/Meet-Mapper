f33aca8f727031f0a114b63fca151fb5a719db84
Flavio Martins <flaviomartins@Flavios-MacBook-Pro.local>
2017-03-24 13:41:14 -0700

    checking possible incomplite results

--- assets/places-logic/places.js
+++ assets/places-logic/places.js
@@ -38,23 +38,25 @@ PlacesConstructor.prototype.search = function(searchInput){
   var searchResult = [];
   
   // Function that saves the result in an array
-  function getResult(results, status){ 
+  function getResult(results, status){
+    console.log(status);
+
     if (status == google.maps.places.PlacesServiceStatus.OK) {
       for (var i = 0; i < 5; i++) {
-
         var myObj ={
           name: results[i].name,
           address: results[i].formatted_address,
           opening: results[i].opening_hours,
           location: {
-            lat: results[i].geometry.location.lat(), 
+            lat: results[i].geometry.location.lat(),
             lng: results[i].geometry.location.lng()
           },
-          photo: results[i].photos[0].html_attributions
+          photo: results[i].photos    // here was the problem o '0' undefind the reason of the problem
         };
-
         searchResult.push(myObj);
       }
+    }else{
+      console.log('on if statement : ' + status);
     }
   }
 
