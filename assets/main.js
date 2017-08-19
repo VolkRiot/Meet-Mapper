4e4c15316b2bdac04662b87d058f8cbe410e5075
Mikhail Metrikin <metrikin@gmail.com>
2017-08-19 15:58:58 -0700

    Clean card html

--- assets/main.js
+++ assets/main.js
@@ -204,27 +204,16 @@ $(document).ready(function () {
         infowindow.close();
       });
 
-      // Build the card html and save to reference for best performance...????
       var panel =
           '<div class="card small">' +
-          '<div class="card-image waves-effect waves-block waves-light">' +
-          '<img class="responsive-img location-image activator" src="assets/style/images/office.jpg">' +
-          '</div>' +
-          '<div class="card-content">' +
-          '<span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>' +
-          '<p><a href="#">This is a link</a></p>' +
-      '</div>' +
+            '<div class="card-content">' +
+              '<span class="card-title grey-text text-darken-4">Card Title<i id="close-button" class="material-icons right">close</i></span>' +
+            '</div>' +
       '<div class="card-reveal">' +
           '<span class="card-title grey-text text-darken-4">Card Title<i id="close-button" class="material-icons right">close</i></span>' +
           '<p>Here is some more information about this product that is only revealed once clicked on.</p>' +
       '</div>' +
-      '</div>';
-
-
-
-      // '<div class="card-reveal">' +
-      //     '<span class="card-title grey-text text-darken-4">Card Title<i id="close-button" class="material-icons right">close</i></span>' +
-      //     '</div>'
+          '</div>';
 
 
       $('#info-card').empty();
